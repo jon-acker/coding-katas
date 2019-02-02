7a49e9f13f4f238a03fd6ab306ee222c5dfa72f0
Jon Acker <jon.acker@worldfirst.com>
2019-02-02 18:09:58 +0000

    Example feature for tennis kata

---
+++ features/js/step-definitions/scoreboard-steps.js
@@ -0,0 +1,22 @@
+const cucumber = require('cucumber');
+
+const Given = cucumber.Given;
+const When = cucumber.When;
+const Then = cucumber.Then;
+
+
+Given('Neither player have scored a point', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+When('{string} scores a point', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('the scoreboard should display {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
